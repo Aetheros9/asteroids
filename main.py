129c1951fbb32508366930f81d7e9e360b8f3fe2
Aetheros9 <akeer939@gmail.com>
2024-10-01 15:22:01 -0600

    finish game

--- main.py
+++ main.py
@@ -1,18 +1,60 @@
 import pygame
 import sys
 from constants import *
+from player import Player
+from asteroid import Asteroid
+from asteroidfield import AsteroidField
+from shot import Shot
 
 def main():
     print("Starting asteroids!")
     pygame.init()
+    clock = pygame.time.Clock()
+
+    updatable = pygame.sprite.Group()
+    drawable = pygame.sprite.Group()
+    asteroids_group = pygame.sprite.Group()
+    shots = pygame.sprite.Group()
+
+    Shot.containers = (shots, updatable, drawable)
+    Player.containers = (updatable, drawable)
+    Asteroid.containers = (asteroids_group, updatable, drawable)
+    AsteroidField.containers = (updatable,)
+
+    dt = 0
+    player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    asteroid_field = AsteroidField()
+
     while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 pygame.quit()
                 sys.exit()
+        
+        for obj in updatable:
+            obj.update(dt)
+
+        for asteroid in asteroids_group:
+            if player.check_collision(asteroid):
+                print("Game over!")
+                sys.exit()
+
+            for shot in shots:
+                if asteroid.check_collision(shot):
+                    shot.kill()
+                    asteroid.split()
+
         screen.fill((0, 0, 0))
+
+        for obj in drawable:
+            obj.draw(screen)
+
         pygame.display.flip()
+        
+        dt = clock.tick(60) / 1000
+        
+
 if __name__ == "__main__":
     print(f"Screen width: {SCREEN_WIDTH}")
     print(f"Screen height: {SCREEN_HEIGHT}")
